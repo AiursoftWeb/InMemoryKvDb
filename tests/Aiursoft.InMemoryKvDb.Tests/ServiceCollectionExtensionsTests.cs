f65f9c54b78cad5f964af8b229bd80a4ad1c196b
Anduin Xue <anduin@aiursoft.com>
2025-12-06 08:09:31 +0000

    Fix redundant nullable suppression warnings

--- tests/Aiursoft.InMemoryKvDb.Tests/ServiceCollectionExtensionsTests.cs
+++ tests/Aiursoft.InMemoryKvDb.Tests/ServiceCollectionExtensionsTests.cs
@@ -24,7 +24,7 @@ public class ServiceCollectionExtensionsTests
         // Assert
         Assert.IsNotNull(store, "LruMemoryStore should be registered and resolved correctly.");
         var id = Guid.NewGuid();
-        var player = store!.GetOrAdd(id);
+        var player = store.GetOrAdd(id);
         Assert.AreEqual("TestPlayer", player.NickName, "Player nickname should match the provided 'onNotFound' logic.");
     }
 
@@ -43,7 +43,7 @@ public class ServiceCollectionExtensionsTests
         // Assert
         Assert.IsNotNull(store, "LruMemoryStoreManualCreated should be registered and resolved correctly.");
         var id = Guid.NewGuid();
-        var player = store!.GetOrAdd(id, iid => new Player(iid, "TestPlayer"));
+        var player = store.GetOrAdd(id, iid => new Player(iid, "TestPlayer"));
         Assert.AreEqual("TestPlayer", player.NickName, "Player nickname should match the manually provided logic.");
     }
 }
